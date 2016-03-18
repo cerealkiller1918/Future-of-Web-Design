c18e4efc6c7688a37eb904d344111f23f3c37e5f
lacychism <lacychismtp@gmail.com>
2016-03-18 13:53:39 -0500

    Added files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ $(document).ready(function(){
     accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
 });
+
+// A setting to initialize dynamically added tooltips, ex when click on svg logos
+$(document).ready(function(){
+  $('.tooltipped').tooltip({delay: 50});
+});
